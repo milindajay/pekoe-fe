f68b44602b87b9bfe740f0e48e8b8487221ed7bc
Milinda John <63205790+milindajay@users.noreply.github.com>
2025-01-25 20:17:42 +0530

    Temporarily removed policies page links

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { SlSocialFacebook, SlSocialYoutube, SlSocialLinkedin } from 'react-icons/sl';
 import { Phone, Mail, MapPin } from 'lucide-react'
 
@@ -116,25 +115,15 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                {/* Uncomment the following section if you want to include the newsletter signup form */}
-                {/* <div className="mt-8">
-                    <h3 className="text-lg font-semibold mb-4">Get in Touch with Updates and News</h3>
-                    <form className="flex flex-col sm:flex-row">
-                        <input type="email" placeholder="Email" className="flex-grow px-4 py-2 rounded-t-md sm:rounded-l-md sm:rounded-t-none focus:outline-none text-gray-800" />
-                        <input type="text" placeholder="Name" className="px-4 py-2 focus:outline-none text-gray-800" />
-                        <button type="submit" className="bg-white text-green-500 px-4 py-2 rounded-b-md sm:rounded-r-md sm:rounded-b-none font-medium hover:bg-gray-100 transition-colors">Sign Up</button>
-                    </form>
-                    <p className="mt-2 text-sm">By signing up you will be accepting Pekoe's Terms and Conditions and acknowledge that my information will be used in accordance with Pekoe's Privacy Policy</p>
-                </div> */}
                 <div className="mt-8 pt-8 border-t border-green-400">
-                    <div className="flex flex-col sm:flex-row justify-between items-center">
+                    {/* <div className="flex flex-col sm:flex-row justify-between items-center">
                         <Image src="/images/TEAAI-WHITE.svg" alt="Pekoe Logo" width={150} height={40} />
                         <div className="text-sm mt-4 sm:mt-0">
                             <Link href="/privacy-policy" className="mr-4 hover:underline">Privacy Policy</Link>
                             <Link href="/cookie-policy" className="mr-4 hover:underline">Cookie Policy</Link>
                             <Link href="/terms-of-use" className="hover:underline">Terms of Use</Link>
                         </div>
-                    </div>
+                    </div> */}
                     <p className="mt-4 text-sm text-center">Copyright © {new Date().getFullYear()} Pekoe. All rights reserved.</p>
                 </div>
             </div>
